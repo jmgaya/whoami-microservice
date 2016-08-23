43f552d0495e151d32fcc03e3f5535331a0c58f4
jmunoz <jmunoz@tuenti.com>
2016-08-23 19:35:25 +0200

    Added proxy support

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.set('port', process.env.PORT || 5000)
 app.get('/', (req, res) => {
   const parsedUA = parser(req.headers['user-agent'])
   res.json({
-    ipaddress: req.ip,
+    ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
     language: req.headers['accept-language'].split(',')[0] || 'UNKNOWN',
     software: `${parsedUA.os.name} ${parsedUA.os.version}`
   })

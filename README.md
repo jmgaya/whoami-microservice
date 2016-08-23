576860f414a63e93b9daa9aa5766e83727300e93
jmunoz <jmunoz@tuenti.com>
2016-08-23 19:27:13 +0200

    Who Am I working

---
+++ README.md
@@ -0,0 +1,3 @@
+# Who Am I Microservice
+
+Who Am I Microservice project from [freeCodeCamp](https://www.freecodecamp.com/challenges/request-header-parser-microservice)
---
+++ src/server.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const express = require('express')
+const parser = require('ua-parser-js')
+const app = express()
+
+app.set('port', process.env.PORT || 5000)
+
+app.get('/', (req, res) => {
+  const parsedUA = parser(req.headers['user-agent'])
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'].split(',')[0] || 'UNKNOWN',
+    software: `${parsedUA.os.name} ${parsedUA.os.version}`
+  })
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`Server listening on port ${app.get('port')}`)
+})
